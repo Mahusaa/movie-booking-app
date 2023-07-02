c5a6f68580d23152b6a2476217a11a3da8f2a183
usamah <hakaro375@gmail.com>
2023-07-02 16:31:47 +0700

    fetch data

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -3,29 +3,50 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
-    <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-        <Link href="/" class="flex items-center">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"><h2 className="text-2xl"><span className="font-bold bg-blue-700 py-1 px-2 rounded-lg mr-1">US</span><span>lify</span></h2></span>
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
+        <Link href="/" className="flex items-center">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <h2 className="text-2xl">
+              <span className="font-bold bg-blue-700 py-1 px-2 rounded-lg mr-1">US</span>
+              <span>lify</span>
+            </h2>
+          </span>
         </Link>
-        <div class="flex items-center md:order-2">
-            <Link href="/login" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</Link>
-            <Link href="/signup" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</Link>
+        <div className="flex items-center md:order-2">
+          <Link href="/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+            Login
+          </Link>
+          <Link href="/signup" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Sign up
+          </Link>
         </div>
-        <div id="mega-menu" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-            <ul class="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
-                <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
-                </li>
-                <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                </li>
-                <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Purchase</a>
-                </li>
-            </ul>
+        <div id="mega-menu" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+          <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                Purchase
+              </a>
+            </li>
+          </ul>
         </div>
-    </div>
-</nav>
+        {/* Mobile menu */}
+        <div className="md:hidden">
+          <button type="button" className="block text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+            Menu
+          </button>
+        </div>
+      </div>
+    </nav>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,4 +1,15 @@
-export default function Page() {
+async function getData() {
+  const res = await fetch("https://seleksi-sea-2023.vercel.app/api/movies");
+
+  if (!res.ok){
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
+
+export default  async function Page() {
+  const data = await getData()
+  console.log(data)
     return <h1 className="text-red-400">Hello, Next.js!</h1>
   }
   
