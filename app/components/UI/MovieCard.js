5915fe36f79911a4115682d213761b5875874a7c
usamah <hakaro375@gmail.com>
2023-07-03 10:57:52 +0700

    remove scrollbar

--- app/components/UI/MovieCard.js
+++ app/components/UI/MovieCard.js
@@ -5,7 +5,7 @@ export default function MovieCard(props) {
   return (
     <div className="absolute bottom-0 left-0 right-0 p-6 bg-white rounded-lg shadow-lg transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 ease-in-out transition-delay-200">
       <div className="flex items-center justify-center">
-        <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full uppercase font-semibold tracking-wide">{props.age}
+        <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full uppercase font-semibold tracking-wide">{`${props.age}+`}
         </span>
         <div className="ml-2 text-gray-600 text-xs font-semibold tracking-wider">{`Release: ${props.release}`}</div>
       </div>
