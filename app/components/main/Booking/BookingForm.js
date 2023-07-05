67eaedb2a47ddd43cb470a362ea0f819ce4a8df7
usamah <hakaro375@gmail.com>
2023-07-05 21:24:17 +0700

    read seats in database

--- app/components/main/Booking/BookingForm.js
+++ app/components/main/Booking/BookingForm.js
@@ -1,8 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-export default function BookingForm({title, ticketPrice}) {
-  console.log(ticketPrice)
+export default function BookingForm({ title, ticketPrice }) {
+  const movieTitle = title
   const [selectedSeats, setSelectedSeats] = useState([]);
+  const [seatsByClient, setSeatsByClient] = useState([]);
+  console.log(selectedSeats)
+
+  useEffect(() => {
+    const fetchSeats = async () => {
+      try {
+        const response = await fetch("https://movie-booking-f84f4-default-rtdb.asia-southeast1.firebasedatabase.app/movie.json");
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+           // Replace with the desired movie title
+          const selectedMovie = Object.values(data).find(movie => movie.title === movieTitle);
+          console.log(selectedMovie);
+          if (selectedMovie) {
+            const selectedSeatsByMovie = selectedMovie.selectedSeats;
+            setSelectedSeats(selectedSeatsByMovie);
+          } else {
+            console.log("No data found for movie:", movieTitle);
+          }
+        } else {
+          console.error('Failed to fetch seat availability:', response.status);
+        }
+      } catch (error) {
+        console.error('Failed by catch to fetch seat availability:', error);
+      }
+    };
+  
+    fetchSeats();
+  }, [movieTitle]);
+  
 
   const handleSeatClick = (seat) => {
     if (selectedSeats.includes(seat)) {
@@ -16,18 +46,21 @@ export default function BookingForm({title, ticketPrice}) {
     return selectedSeats.includes(seat);
   };
 
-  const totalPrice = ticketPrice*selectedSeats.length
-  console.log(totalPrice)
+  const totalPrice = ticketPrice * seatsByClient.length;
+  console.log(totalPrice);
 
   const handleConfirmBooking = async () => {
-    await fetch("https://movie-booking-f84f4-default-rtdb.asia-southeast1.firebasedatabase.app/movie.json", {
-      method: "POST",
-      body: JSON.stringify({
-        title,
-        totalPrice,
-        selectedSeats,
-      })
-    })
+    await fetch(
+      "https://movie-booking-f84f4-default-rtdb.asia-southeast1.firebasedatabase.app/movie.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          totalPrice,
+          selectedSeats,
+        }),
+      }
+    );
   };
 
   return (
@@ -40,9 +73,9 @@ export default function BookingForm({title, ticketPrice}) {
               key={seat}
               onClick={() => handleSeatClick(seat)}
               className={`w-10 h-10 rounded-lg cursor-pointer text-white font-bold flex items-center justify-center ${
-                isSeatSelected(seat) ? 'bg-green-500' : 'bg-gray-500'
+                isSeatSelected(seat) ? "bg-green-500" : "bg-gray-500"
               }`}
-              style={{ margin: '2px' }} // Added margin for spacing
+              style={{ margin: "2px" }} // Added margin for spacing
             >
               {seat}
             </div>
@@ -54,15 +87,13 @@ export default function BookingForm({title, ticketPrice}) {
           <h2 className="text-xl font-semibold mb-4">Order Summary</h2>
           <span className="font-semibold">{`Movie :${title}`}</span>
           <div className="mb-2">
-          <span className="font-semibold">Selected Seats:</span>{' '}
-            {selectedSeats.join(', ')}
+            <span className="font-semibold">Selected Seats:</span>{" "}
+            {selectedSeats.join(", ")}
           </div>
           <div className="mb-4">
             <span className="font-semibold">Total Price:</span> Rp {totalPrice}
           </div>
-          <button onClick={handleConfirmBooking}>
-            Confirm Booking
-          </button>
+          <button onClick={handleConfirmBooking}>Confirm Booking</button>
         </div>
       </div>
     </div>
